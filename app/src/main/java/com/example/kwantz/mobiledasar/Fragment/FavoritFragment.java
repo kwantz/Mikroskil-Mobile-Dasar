dedb1be8011e150a232ba5a63f35823070d1878e
Erick Kwantan <erick.kwantan@mikroskil.ac.id>
2017-11-28 09:37:11 +0700

    Tambah Icon + Edit Fragment

--- app/src/main/java/com/example/kwantz/mobiledasar/Fragment/FavoritFragment.java
+++ app/src/main/java/com/example/kwantz/mobiledasar/Fragment/FavoritFragment.java
@@ -7,10 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.kwantz.mobiledasar.R;
 
 public class FavoritFragment extends Fragment {
+
+    private View view;
+    private ImageView notifBtn, chatBtn;
 
     public FavoritFragment() {
         // Required empty public constructor
@@ -19,6 +24,35 @@ public class FavoritFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorit, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        initializationVariable();
+        initializationEvent();
+
+        return view;
+    }
+
+    private void initializationVariable () {
+        notifBtn = (ImageView) view.findViewById(R.id.btn_notif);
+        chatBtn = (ImageView) view.findViewById(R.id.btn_chat);
+    }
+
+    private void initializationEvent () {
+        notifBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick (View view) {
+                Toast toast = Toast.makeText(getActivity(), "Notifikasi", Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
+            }
+        });
+
+        chatBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick (View view) {
+                Toast toast = Toast.makeText(getActivity(), "Chat", Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kwantz/mobiledasar/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/kwantz/mobiledasar/Fragment/HomeFragment.java
@@ -1,16 +1,22 @@
 package com.example.kwantz.mobiledasar.Fragment;
 
 import android.content.Context;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.kwantz.mobiledasar.R;
 
 public class HomeFragment extends Fragment {
+
+    private View view;
+    private ImageView notifBtn, chatBtn;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -19,6 +25,35 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        initializationVariable();
+        initializationEvent();
+
+        return view;
+    }
+
+    private void initializationVariable () {
+        notifBtn = (ImageView) view.findViewById(R.id.btn_notif);
+        chatBtn = (ImageView) view.findViewById(R.id.btn_chat);
+    }
+
+    private void initializationEvent () {
+        notifBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick (View view) {
+                Toast toast = Toast.makeText(getActivity(), "Notifikasi", Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
+            }
+        });
+
+        chatBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick (View view) {
+                Toast toast = Toast.makeText(getActivity(), "Chat", Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
+            }
+        });
     }
 }
