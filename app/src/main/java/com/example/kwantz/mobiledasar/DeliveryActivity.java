bd8aa3dba97d23d67dcd03e003dbb85c6ecb27df
silfiL <silfilangie04@gmail.com>
2018-01-06 17:41:22 +0700

    Finishing first page. Will need a little fixes later

--- app/src/main/java/com/example/kwantz/mobiledasar/DeliveryActivity.java
+++ app/src/main/java/com/example/kwantz/mobiledasar/DeliveryActivity.java
@@ -3,16 +3,57 @@ package com.example.kwantz.mobiledasar;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class DeliveryActivity extends AppCompatActivity {
+    private ImageView back;
+    private TextView cttn;
+    private CheckBox checkPelapak;
+    private LinearLayout Pelapak, cttnPelapak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery);
+
+        cttnPelapak = (LinearLayout)findViewById(R.id.cttnPelapak);
+        Pelapak = (LinearLayout)findViewById(R.id.pelapak);
+
+        cttn = (TextView)findViewById(R.id.tv_cttnPelapak);
+        cttn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                cttn.setVisibility(View.GONE);
+                cttnPelapak.setVisibility(View.VISIBLE);
+            }
+        });
+
+        back = (ImageView) findViewById(R.id.back_btn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                onBackPressed();
+            }
+        });
+
+        checkPelapak = (CheckBox)findViewById(R.id.check_pelapak);
+        checkPelapak.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b == true)
+                    Pelapak.setVisibility(View.VISIBLE);
+                else
+                    Pelapak.setVisibility(View.GONE);
+            }
+        });
 
         Spinner numSpinner = (Spinner) findViewById(R.id.order_quantity);
         Spinner deliverySpinner = (Spinner) findViewById(R.id.delivery_service);
